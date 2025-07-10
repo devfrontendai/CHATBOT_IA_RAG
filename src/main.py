d83c79c1d5983d6c3d807560751fd2e11a338a92
dcamacho <dcamacho@tiprotec.com>
2025-07-10 15:07:33 -0600

    update: se ajusta swagger, se quita endpoint extrade productos, solo se usara historial

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from api import router
 from fastapi.middleware.cors import CORSMiddleware
-from endpoints.scripts.producto import router as productos_router
+#from endpoints.scripts.producto import router as productos_router
 from endpoints.scripts.polizas_canceladas import router as canceladas_router
 from endpoints.scripts.proximas_vigencias import router as vigencias_router
 from endpoints.scripts.historial_productos import router as historial_router
@@ -16,8 +16,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(router)
-app.include_router(productos_router)
+#app.include_router(productos_router)
+app.include_router(historial_router)
 app.include_router(canceladas_router)
 app.include_router(vigencias_router)
-app.include_router(historial_router)
 app.include_router(vigencias_router)
